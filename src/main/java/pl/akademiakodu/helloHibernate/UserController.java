a912655bba1d3c9f10e46061c73b62599cd157d0
Dawid Czartoszewski <dawid.czartoszewski@gmail.com>
2018-07-24 23:03:27 +0200

    first commit

---
+++ src/main/java/pl/akademiakodu/helloHibernate/UserController.java
@@ -0,0 +1,33 @@
+package pl.akademiakodu.helloHibernate;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserRepository userDao;
+
+    @GetMapping("/")
+    public String add(ModelMap modelMap){
+        modelMap.put("user",new User());
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String addUser(@ModelAttribute User user){
+        userDao.save(user);
+        return "redirect:/all";
+    }
+    @GetMapping("/all")
+    public String getAll(ModelMap modelMap){
+        modelMap.put("users",userDao.findAll());
+        return "/all";
+    }
+}
---
+++ src/main/java/pl/akademiakodu/helloHibernate/UserRepository.java
@@ -0,0 +1,13 @@
+package pl.akademiakodu.helloHibernate;
+
+import org.springframework.data.repository.CrudRepository;
+
+import pl.akademiakodu.helloHibernate.User;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+
+}
